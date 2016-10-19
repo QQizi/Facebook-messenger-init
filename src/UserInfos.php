4e9312b0a8f0234b0e84c26ab686dba19d622d0e
QQizi <quentin.mangin@gmail.com>
2016-10-18 21:32:32 -0400

    user get infos resolve token

--- src/UserInfos.php
+++ src/UserInfos.php
@@ -12,13 +12,17 @@ class UserInfos {
 
     public $user;
 
-    function __construct($sender = NULL){
+    private $facebookToken;
+
+    function __construct($facebookToken = NULL, $sender = NULL){
+
+        $this->facebookToken        =   $facebookToken;
 
         $this->user                 =   new \stdClass();
         $this->user->id             =   $sender;
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/v2.6/".$sender."?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=".ACCESS_TOKEN);
+        curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/v2.6/".$sender."?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=".$this->facebookToken);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $this->user->infos = json_decode(curl_exec($ch));
